1c65c423cd3e758b2f925bf799d50c4bc7eafbeb
Arnaud <arnaud.decourt@etu.univ-lyon1.fr>
2024-10-10 16:40:17 +0200

    feat : 4.1 ajout modèle Review

--- src/models/review.model.ts
+++ src/models/review.model.ts
@@ -2,16 +2,42 @@ import { DataTypes, Model } from "sequelize";
 import sequelize from "../config/database"; // Connexion à la base de données
 
 export interface ReviewAttributes {
+  id?: number;
+  game_id: number;
+  rating: number;
+  review_text: string; 
 }
 
 export class Review
   extends Model<ReviewAttributes>
   implements ReviewAttributes
 {
+  public id!: number;
+  public game_id!: number;
+  public rating!: number;
+  public review_text!: string;
 }
 
 Review.init(
-  {},
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    review_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
   {
     sequelize,
     tableName: "reviews",
