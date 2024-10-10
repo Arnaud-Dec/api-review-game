79697c68611075529e6d7255f7bcd7d98862f3d1
Arnaud <arnaud.decourt@etu.univ-lyon1.fr>
2024-10-10 17:10:40 +0200

    feat : 4.3 getall Review #2

--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -1,9 +1,17 @@
 import { Review } from "../models/review.model";
+import { Game } from "../models/game.model";
 
 export class ReviewService {
 
     public async getAllReviews(): Promise<Review[]> {
-        return await Review.findAll();
+        return await Review.findAll({
+            include: [
+                {
+                    model: Game,
+                    as: "game",
+                },
+            ],
+        });
     }
 }
 export const reviewService = new ReviewService();
