188cf0c86743233c89d6884b840dd8e7e4748c0a
Arnaud <arnaud.decourt@etu.univ-lyon1.fr>
2024-10-10 17:26:17 +0200

    feat : 4.5 post Review

--- src/controllers/review.controller.ts
+++ src/controllers/review.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Route, Tags , Get} from "tsoa";
+import { Controller, Route, Tags , Get , Post , Body} from "tsoa";
 import { ReviewDTO } from "../dto/review.dto";
 import { reviewService } from "../services/review.service";
 import { Review } from "../models/review.model";
+import { notFound } from "../error/NotFoundError";
 
 @Route("reviews")
 @Tags("Reviews")
@@ -15,6 +16,18 @@ export class ReviewController extends Controller {
     public async getReviewById(id: number): Promise<ReviewDTO | null> {
       const review = await reviewService.getReviewById(id);
       return review;
+    }
+
+    @Post("/")
+    public async createReview(
+      @Body() requestBody: ReviewDTO
+    ): Promise<ReviewDTO> {
+      const { game, rating, content } = requestBody;
+      if(!game?.id){
+        notFound("Game "+game?.id);
+      }else{
+        return reviewService.createReview(game!.id!, rating, content);
+      }
     }
 
   }
--- src/services/review.service.ts
+++ src/services/review.service.ts
@@ -1,5 +1,6 @@
 import { Review } from "../models/review.model";
 import { Game } from "../models/game.model";
+import { notFound } from "../error/NotFoundError";
 
 export class ReviewService {
 
@@ -24,6 +25,21 @@ export class ReviewService {
             ],
         });
     }
+
+    public async createReview(
+        game_id: number,
+        rating: number,
+        content: string
+    ): Promise<Review> {
+        const game = await Game.findByPk(game_id);
+        if (!game) {
+            notFound("Game " + game_id);
+        }else{
+            return Review.create({ game_id, rating, content });
+        }
+    }
+        
+    
 
 }
 export const reviewService = new ReviewService();
